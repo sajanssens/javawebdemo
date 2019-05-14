6dcb49f0160eb7468ebd49f4f5b417ca7e657b0a
bramjanssens <bram.janssens@infosupport.com>
2019-05-14 16:33:35 +0200

    Initial

---
+++ src/main/java/com/example/MyServlet.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println("bram!!!!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println("POST bram");
+    }
+}
