80ceebb6744c2187e84e71fc9b5b97ec73691ac1
bramjanssens <bram.janssens@infosupport.com>
2019-05-16 12:27:07 +0200

    RequestLogFilter added

---
+++ src/main/java/com/example/filters/RequestLogFilter.java
@@ -0,0 +1,16 @@
+package com.example.filters;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class RequestLogFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        long start = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        long duration = System.currentTimeMillis() - start;
+
+        System.out.println(("Request took (ms): " + duration));
+    }
+}
