8bedcbda418c405df30585149030a2ae10d94ccb
bramjanssens <bram.janssens@infosupport.com>
2019-05-16 15:00:12 +0200

    ActiveUsersListener added.

---
+++ src/main/java/com/example/listeners/ActiveUsersListener.java
@@ -0,0 +1,22 @@
+package com.example.listeners;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class ActiveUsersListener implements HttpSessionListener {
+    private static int count;
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        count++;
+        System.out.println("ActiveUsers: " + count);
+        se.getSession().setAttribute("activeUsers", count);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        count--;
+        System.out.println("ActiveUsers: " + count);
+        se.getSession().setAttribute("activeUsers", count);
+    }
+}
--- src/main/webapp/admin/index.jsp
+++ src/main/webapp/admin/index.jsp
@@ -2,6 +2,7 @@
 <body>
 <h2>Hello admin!</h2>
 <h3>Logged in as ${username}.</h3>
+<h3>Active users ${activeUsers}.</h3>
 <a href="../logout.jsp">Click to Logout </a>
 </body>
 </html>
