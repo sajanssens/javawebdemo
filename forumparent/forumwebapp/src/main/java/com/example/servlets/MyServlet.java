32d41d7f1914d5788b67e2f0170f32efa4feeef9
bramjanssens <bram.janssens@infosupport.com>
2021-05-24 11:56:00 +0200

    Some improvements.

--- forumparent/forumwebapp/src/main/java/com/example/servlets/MyServlet.java
+++ forumparent/forumwebapp/src/main/java/com/example/servlets/MyServlet.java
@@ -1,18 +1,54 @@
 package com.example.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MyServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("MyServlet.GET");
+
+        writeToResponse(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("MyServlet.POST");
+    }
+
+    private void writeToResponse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Write to the response, for example either:
+
+        // 1) text
+        // resp.getWriter().write("Hello");
+
+        // 2) json
+        // resp.getWriter().write("{\"message\": \"Hello\"}");
+
+        // 3) HTML
+        // resp.getWriter().write(
+        //         "<html>\n" +
+        //                 "    <body>\n" +
+        //                 "        <h2>Hello!</h2>\n" +
+        //                 "        <a href=\"index.jsp\">Back</a>\n" +
+        //                 "    </body>\n" +
+        //                 "</html>");
+
+        // 4) HTML with text blocks (Java 15+)
+        // resp.getWriter().write("""
+        //         <html>
+        //             <body>
+        //                 <h2>Hello text blocks!</h2>
+        //                 <a href="index.jsp">Back</a>
+        //             </body>
+        //         </html>
+        //         """);
+
+        // 5) Using a jsp
+        req.getRequestDispatcher("myservlet.jsp").forward(req, resp);
     }
 }
