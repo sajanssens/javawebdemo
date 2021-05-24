339d8c871b74ab54f59a76cbe0e892453176ea1a
bramjanssens <bram.janssens@infosupport.com>
2021-05-24 11:12:40 +0200

    Some improvements.

--- forumparent/forumrunner/src/main/java/com/example/Runner.java
+++ forumparent/forumrunner/src/main/java/com/example/Runner.java
@@ -12,23 +12,36 @@ import java.io.File;
 
 public class Runner {
 
+    // Deploys forumwebapp to http://localhost:8082/
+    // Hot deploy of classes DOES NOT WORK! You need to restart the runner for testing your changes.
+
     public static void main(String[] args) throws LifecycleException, ServletException {
-        String webappDirLocation = "forumwebapp/src/main/webapp/";
-        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        String projectRoot = "forumparent/forumwebapp/";
+        String webappLocation = projectRoot + "src/main/webapp/";
+        String classesLocation = projectRoot + "target/classes";
+
+        System.out.println("Configuring app with basedir: " + new File(projectRoot).getAbsolutePath());
 
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8082);
-        tomcat.addRole("bram", "user");
-        tomcat.addRole("bram", "admin");
-        tomcat.addUser("bram", "bram");
 
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("/forum", new File(webappDirLocation).getAbsolutePath());
+        // Add some roles and users for testing the secured part of the app:
+        // tomcat.addRole("bram", "user");
+        // tomcat.addRole("bram", "admin");
+        // tomcat.addUser("bram", "bram");
 
+        // This is equivalent to adding a web application to Tomcat's webapps directory:
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("", new File(webappLocation).getAbsolutePath());
+
+        // Map the WEB-INF/classes to target/classes
         WebResourceRoot resources = new StandardRoot(ctx);
-        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
-                new File("forumwebapp/target/classes").getAbsolutePath(), "/"));
+        resources.addPreResources(new DirResourceSet(resources,
+                "/WEB-INF/classes",
+                new File(classesLocation).getAbsolutePath(),
+                "/"));
         ctx.setResources(resources);
 
+        // Start tomcat and wait until it is finished.
         tomcat.start();
         tomcat.getServer().await();
     }
--- forumparent/forumwebapp/src/main/java/com/example/filters/StopEverythingFilter.java
+++ forumparent/forumwebapp/src/main/java/com/example/filters/StopEverythingFilter.java
@@ -8,7 +8,8 @@ import javax.servlet.ServletResponse;
 // Turn on to bypass everything and stop the application from working
 // @WebFilter("/*")
 public class StopEverythingFilter implements Filter {
-    @Override public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
         System.out.println("STOPPING THE WORLD....");
         // chain.doFilter();
     }
